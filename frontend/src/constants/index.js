75b8221d79083ea78dc922087d01f4cc7de001c9
Sesante1 <jomelsesante989@gmail.com>
2026-05-24 23:44:39 +0800

    Added hero seciton with image

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { Text: "Ideas", imagePath: "/images/ideas.svg" },
+  { Text: "Concepts", imagePath: "/images/concepts.svg" },
+  { Text: "Designs", imagePath: "/images/designs.svg" },
+  { Text: "Code", imagePath: "/images/code.svg" },
+  { Text: "Ideas", imagePath: "/images/ideas.svg" },
+  { Text: "Concepts", imagePath: "/images/concepts.svg" },
+  { Text: "Designs", imagePath: "/images/designs.svg" },
+  { Text: "Code", imagePath: "/images/code.svg" },
+];
--- frontend/src/sections/hero.jsx
+++ frontend/src/sections/hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { words } from "../constants/index";
+import Button from "../components/button";
 
 const hero = () => {
   return (
@@ -12,13 +14,49 @@ const hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-              <h1>Shaping</h1>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imagePath}
+                          alt={word.Text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.Text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Jomel, a passionate software developer with a knack for{" "}
+              <br />
+              turning innovative ideas into real-world applications.
+            </p>
+
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my work"
+            />
           </div>
         </header>
-        {/* right 3d model or image */}
+        {/* Profile Picture */}
+        <figure>
+          <div className="rounded-md">
+            <img src="/images/GradPic.jpg" alt="Profile Picture" />
+          </div>
+        </figure>
       </div>
     </section>
   );
